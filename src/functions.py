3137a6ca95f1424edfd472d298ce407ffe8cd0f5
LouisBove84 <LouisBove84@gmail.com>
2021-01-12 11:28:47 -0700

    change

--- src/functions.py
+++ src/functions.py
@@ -364,6 +364,7 @@ class NLP:
         plt.title('Regression Model Comparison', size = 25)
         plt.xticks(size = 20)
         plt.yticks(size = 20)
+        plt.xlim(0,1)
         plt.grid()
         plt.show()
 
